f77b61608a57e239a3c4ec737379b496fdf107c9
Megan Tan <meganytan@gmail.com>
2023-11-27 03:44:36 -0600

    change main.go port for prometheus to 9090

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 
     http.Handle("/metrics", promhttp.Handler()) // Expose the metrics
     go makeAPICall()
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":9090", nil))
 
     for {
 		// build and fine-tune functions to pull data from different data sources
